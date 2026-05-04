66836cdbc1aa112a3e255b9aaae7885f6522bd6d
evinirmalaaaa <164207721+evinirmalaaaa@users.noreply.github.com>
2026-05-04 09:09:45 +0700

    init(PBI-06): create ReportController for handling program reports and exports

---
+++ SPRINT 1/backend/app/Http/Controllers/ReportController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ReportController extends Controller
+{
+    /**
+     * Controller ini bertanggung jawab untuk menangani PBI-06 (Laporan & Ekspor Data Program).
+     */
+}
